91d72a591658dfc17e9cec7ad35faf0bda5f4747
Gloria Vela <gloria.laura.vela@gmail.com>
2019-02-22 19:08:55 -0600

    Completed

---
+++ src/components/CourseList.js
@@ -0,0 +1,74 @@
+import React, { Component } from 'react';
+import Grid from '@material-ui/core/Grid';
+import TextField from '@material-ui/core/TextField';
+import * as contentful from 'contentful';
+import Course from '../components/Course';
+
+const SPACE_ID = 's1q9ned0corp';
+const ACCESS_TOKEN = '2ab6bb5de50d7a7e6ba9882b6bd2dba33bbd640111c61159861762282a074be4';
+
+// The connection to the Contentful space is stored in client.
+const client = contentful.createClient({
+    space: SPACE_ID,
+    accessToken: ACCESS_TOKEN
+});
+
+class CoursesList extends Component {
+    state = {
+        courses: [],
+        searchString: ''
+    }
+    constructor() {
+        super()
+        this.getCourses()
+    }
+    // To retrieve the list of courses from this connection the method getCourses is added to the component class:
+    getCourses = () => {
+        client.getEntries({
+            content_type: 'course',
+            query: this.state.searchString
+        })
+        .then((response) => {
+            this.setState({courses: response.items})
+            console.log(this.state.courses)
+        })
+        .catch((error) => {
+          console.log("Error occurred while fetching Entries")
+          console.error(error)
+        })
+    }
+    onSearchInputChange = (event) => {
+        console.log("Search changed ..." + event.target.value)
+        if (event.target.value) {
+            this.setState({searchString: event.target.value})
+        } else {
+            this.setState({searchString: ''})
+        }
+        this.getCourses()
+    }
+    render() {
+        // The component’s output is defined by the JSX code which is returned by the render method
+        return (
+            <div>
+                { this.state.courses ? (
+                    <div>
+                        <TextField style={{padding: 24}}
+                            id="searchInput"
+                            placeholder="Search for Courses"   
+                            margin="normal"
+                            onChange={this.onSearchInputChange}
+                            />
+                        <Grid container spacing={24} style={{padding: 24}}>
+                            { this.state.courses.map(currentCourse => (
+                                <Grid item xs={12} sm={6} lg={4} xl={3}>
+                                    <Course course={currentCourse} />
+                                </Grid>
+                            ))}
+                        </Grid>
+                    </div>
+                ) : "No courses found" }
+            </div>
+        )
+    }
+}
+export default CoursesList;
